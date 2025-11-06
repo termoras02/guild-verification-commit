c54464ad11330d506306f2279cfebb29170486be
ritosss <113219639+termoras02@users.noreply.github.com>
2025-11-06 05:31:31 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:31:26.454Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-06T02:31:30.158Z
+Commit: Fix broken links in documentation
