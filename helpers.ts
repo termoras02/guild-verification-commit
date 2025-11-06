86a8c86459e14487dd509d879d095eac88fb284b
ritosss <113219639+termoras02@users.noreply.github.com>
2025-11-06 05:17:48 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:17:39.670Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T02:17:47.696Z
+Commit: Resolve null pointer exception in user service
