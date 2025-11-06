036c0d5bfdf56e32c57c0429b978b1bee0b0ed12
ritosss <113219639+termoras02@users.noreply.github.com>
2025-11-06 05:38:27 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:38:04.018Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T02:38:26.270Z
+Commit: Fix CSS styling bug on mobile devices
