85bad61816e54971da8fac6d1be81ebbac27675b
ritosss <113219639+termoras02@users.noreply.github.com>
2025-11-06 05:16:10 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:09:50.175Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-06T02:16:09.248Z
+Commit: Fix CSS styling bug on mobile devices
