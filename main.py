a26ac54e3b2787a72c3e15d1924ce38b2aca537c
ritosss <113219639+termoras02@users.noreply.github.com>
2025-11-06 05:16:13 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:16:10.984Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T02:16:12.488Z
+Commit: Fix broken links in documentation
