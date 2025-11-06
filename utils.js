c64481b5d4638926faaba2efc82c3b6b3f154172
ritosss <113219639+termoras02@users.noreply.github.com>
2025-11-06 05:29:47 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:29:34.387Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-06T02:29:45.956Z
+Commit: Add export functionality for reports
